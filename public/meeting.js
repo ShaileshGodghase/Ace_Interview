26557e5edca5b15c6c9c9402e5fddc862707cd29
Shailesh Godghase <shaileshgodghase19@gmail.com>
2022-11-28 20:32:02 +0530

    Added IceServers

--- public/meeting.js
+++ public/meeting.js
@@ -26,6 +26,30 @@ var peer = new Peer(undefined, {
    path: "/peerjs",
    host: "/",
    port: "443",
+   config: {
+      'iceServers': [
+         { url: 'stun:stun01.sipphone.com' },
+         { url: 'stun:stun.ekiga.net' },
+         { url: 'stun:stunserver.org' },
+         { url: 'stun:stun.softjoys.com' },
+         { url: 'stun:stun.voiparound.com' },
+         { url: 'stun:stun.voipbuster.com' },
+         { url: 'stun:stun.voipstunt.com' },
+         { url: 'stun:stun.voxgratia.org' },
+         { url: 'stun:stun.xten.com' },
+         {
+            url: 'turn:192.158.29.39:3478?transport=udp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808'
+         },
+         {
+            url: 'turn:192.158.29.39:3478?transport=tcp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808'
+         }
+      ]
+   },
+   debug: 3
 });
 
 let myVideoStream;
